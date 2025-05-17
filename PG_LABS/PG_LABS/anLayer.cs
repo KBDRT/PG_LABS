fcc136c693c4e2479cbfacd438811834a537249e
KBDRT <midaskbadrat@gmail.com>
2025-05-17 16:22:01 +0300

    test

--- PG_LABS/PG_LABS/anLayer.cs
+++ PG_LABS/PG_LABS/anLayer.cs
@@ -136,13 +136,6 @@ namespace PG_LABS
                 {
                     if (DrawPlace[ax, bx, 3] != 1)
                     {
-                        MessageBox.Show(
-                         "Выберите один из вариантов",
-                         "Сообщение",
-                         MessageBoxButtons.YesNo,
-                         MessageBoxIcon.Information,
-                         MessageBoxDefaultButton.Button1,
-                         MessageBoxOptions.DefaultDesktopOnly);
                         count++;
                     }
                 }
